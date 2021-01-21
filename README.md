852917bf82f0a37d664b6feeb65778018048341f
Mike <lmk123568@qq.com>
2021-01-21 22:10:45 +0800

    第一次修改

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# OD-Confluence
-目标检测中替代传统NMS的后处理方式
+# OD Confluence
+
+目标检测中替代传统NMS的后处理方式，个人简单复现，原作作者暂时还没开源
+
+<img src="md\11.png" alt="11" style="zoom:50%;" />
+
+<img src="md\12.png" alt="12" style="zoom:50%;" />
+
+
+
+------
+
+![1](\md\1.png)
+
+paper：https://arxiv.org/abs/2012.00257
+
+## Introduction
+
+<img src="md\2.png" alt="2" style="zoom:75%;" />
+
+​	本文提出了一种在目标检测中的边界框选择和抑制任务中替代 NMS 的新颖方法。它提出了Confluence，一种不仅仅依赖于 confidence scores 来选择最佳边界框的算法，也不依赖于 IoU 来消除误报。使用“曼哈顿距离”，它选择最接近群集中其他所有边界框的边界框，并删除高度融合的相邻框。因此，Confluence基于与Greedy  NMS及其变体根本不同的理论原理，代表着边界框选择和抑制的范式转变。使用MS COCO和PASCAL VOC  2007数据集，在RetinaNet，YOLOv3和Mask-RCNN上对融合进行了实验验证。使用具有挑战性的 0.50:0.95  mAP评估指标，汇合在两个mAP上均优于Greedy  NMS，并且在两个数据集上的召回率均优于。在每个检测器和数据集上，mAP改善了0.3-0.7％，而召回率则提高了1.4-2.5％。提供了Greedy  NMS和Confluence算法的理论比较，并通过广泛的定性结果分析来支持定量结果。此外，跨mAP阈值的灵敏度分析实验支持以下结论：Confluence比NMS更可靠。
+
+## Methodology
+
+曼哈顿距离：
+
+![3](\md\3.png)
+
+任意两个边界框接近度P：
+
+![4](\md\4.png)
+
+如图所示：
+
+<img src="\md\5.png" alt="5" style="zoom:80%;" />
+
+实际上对象及其相应边界框具有不同大小，需要进一步Normalization
+
+<img src="md\6.png" alt="6"  />
+
+伪代码：
+
+![7](\md\7.png)
+
+设集合B：一个class下所有bbox，集合C：提取的最终bbox
+
+1. 提取一个class的所有bbox
+2. 对所有bbox的每个bbox，计算这个bbox与其他bbox的P
+   1. P<2的bbox与原bbox一起看作cluster
+   2. 在这cluster计算WP
+   3. 选取这个bbox最小的WP
+3. 从B中选出具有最小的WP的bbox，将这个提取的bbox加入C，同时从B中删除这个bbox属于的cluster中所有bbox
+4. 重复迭代上述过程，直到集合B为0
+
+
+
+## Results
+
+![8](md\8.png)
+
---
+++ confluence.py
@@ -0,0 +1,124 @@
+import numpy as np
+import cv2
+
+
+# bbox_original = np.loadtxt('woman_bbox.txt')
+# path = 'woman.jpg'
+
+# bbox_original = np.loadtxt('cat_bbox.txt')
+# path = 'cat.jpg'
+
+bbox_original = np.loadtxt('person_bbox.txt')
+path = 'person.jpg'
+
+def imshow(path, bbox):
+
+    img = cv2.imread(path)
+
+    bbox1 = bbox[:, :4].astype(int)
+
+    min_point = [tuple(x) for x in bbox1[:, :2].tolist()]
+
+    max_point = [tuple(x) for x in bbox1[:, 2:4].tolist()]
+
+    for i in range(len(bbox)):
+        cv2.rectangle(img, min_point[i], max_point[i], (0, 255, 0), 2)
+
+    cv2.imshow('img', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+
+def norm(bbox1, bbox2):
+    x1min, y1min, x1max, y1max = bbox1[0], bbox1[1], bbox1[2], bbox1[3]
+    x2min, y2min, x2max, y2max = bbox2[0], bbox2[1], bbox2[2], bbox2[3]
+
+    lx = max(x1min, x1max, x2min, x2max)-min(x1min, x1max, x2min, x2max)
+    ly = max(y1min, y1max, y2min, y2max)-min(y1min, y1max, y2min, y2max)
+
+    xmin = min(x1min, x1max, x2min, x2max)
+    ymin = min(y1min, y1max, y2min, y2max)
+
+    x1min = (x1min-xmin)/lx
+    y1min = (y1min-ymin)/ly
+    x2min = (x2min-xmin)/lx
+    y2min = (y2min-ymin)/ly
+    x1max = (x1max-xmin)/lx
+    y1max = (y1max-ymin)/ly
+    x2max = (x2max-xmin)/lx
+    y2max = (y2max-ymin)/ly
+
+    p = np.absolute(x1min-x2min)+np.absolute(x1max-x2max) + \
+        np.absolute(y1min-y2min)+np.absolute(y1max-y2max)
+
+    return p
+
+
+
+def confluence(bbox,threshold):
+    
+   
+    print(30*'-')
+    
+    bbox_num=[i for i in range(len(bbox))]#对各个bbox进行编号
+    
+    keep = []
+
+    while len(bbox_num) > 0:
+        
+        score_min = []#存放每一轮每个bbox最小WP
+        loc_set=[]#存放每个bbox的P<阈值的bbox编号
+        
+        for i in bbox_num:
+            score = []#存放bbox[i]的所有WP，然后选出最小
+            loc=[]#存放P<threshold的bbox编号
+            for j in bbox_num:
+                P = norm(bbox[i], bbox[j])
+                if P<threshold:
+                    loc.append(j)
+                    
+                if P < 2 and P != 0:
+                    WP = P/bbox[i][4]
+                    score.append(WP)
+                    
+                    
+            score_min.append(round(min(score), 3))
+            loc_set.append(loc)
+            
+        print('各个bbox最低WP:', score_min)
+        print('各bbox的 P<treshold 的另一个bbox编号:',loc_set)
+       
+        
+        index = score_min.index(min(score_min))
+        bbox_index=bbox_num[index]
+        print('最低score bbox编号:',bbox_index)
+        
+        keep.append(bbox_index)
+        print('最终bbox集合:',keep)
+        print('要删除的bbox编号',loc_set[index])
+        
+        for i in loc_set[index]:
+            bbox_num.remove(i)
+            
+        
+        print('剩下bbox:',bbox_num)
+        
+        
+        
+        if len(bbox_num)==1:
+            keep.append(bbox_num[0])
+            print('剩下1个，并入最终bbox',keep)
+            break
+        
+        print(30*'-')
+   
+    bbox_process = bbox[keep]
+    
+    return bbox_process
+
+
+imshow(path, bbox_original)
+
+imshow(path, confluence(bbox_original,0.6))
+
